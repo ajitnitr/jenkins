7c56ad51150f35e64dbadd1784371e70fff5c0dd
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-01-29 23:33:44 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -45,7 +45,7 @@ folder('CI-Pipelines') {
   description('CI-Pipelines')
 }
 
-def COMPONENTS = ["cart", "catalogue", "payment", "shipping", "user", "dispatch"]
+def COMPONENTS = ["frontend","cart", "catalogue", "payment", "shipping", "user", "dispatch"]
 
 def SIZE =  COMPONENTS.size -1
 
