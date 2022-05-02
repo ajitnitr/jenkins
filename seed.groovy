ccd18a10d033ae93661e1b1ab5d031f8fbaf4c2f
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-05-03 00:04:21 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -1,24 +1,3 @@
-//pipelineJob('frontend') {
-//  configure { flowdefinition ->
-//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//        'userRemoteConfigs' {
-//          'hudson.plugins.git.UserRemoteConfig' {
-//            'url'('https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps60/_git/frontend')
-//          }
-//        }
-//        'branches' {
-//          'hudson.plugins.git.BranchSpec' {
-//            'name'('*/main')
-//          }
-//        }
-//      }
-//      'scriptPath'('Jenkinsfile')
-//      'lightweight'(true)
-//    }
-//  }
-//}
-
 pipelineJob('roboshop-ansible') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
@@ -45,23 +24,33 @@ folder('CI-Pipelines') {
   description('CI-Pipelines')
 }
 
-pipelineJob('CI-Pipelines/cart') {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/ajitnitr/cart.git')
+def COMPONENTS = ["cart", "catalogue", "payment", "shipping", "user", "dispatch"]
+
+def SIZE =  COMPONENTS.size -1
+
+for(i in 0..SIZE) {
+  def j = COMPONENTS[i]
+  pipelineJob("CI-Pipelines/${j}") {
+    configure { flowdefinition ->
+      flowdefinition << delegate.'definition'(class: 'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition', plugin: 'workflow-cps') {
+        'scm'(class: 'hudson.plugins.git.GitSCM', plugin: 'git') {
+          'userRemoteConfigs' {
+            'hudson.plugins.git.UserRemoteConfig' {
+              'url'("https://github.com/ajitnitr/${j}.git")
+            }
+          }
+          'branches' {
+            'hudson.plugins.git.BranchSpec' {
+              'name'('*/main')
+            }
           }
         }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
+        'scriptPath'('Jenkinsfile')
+        'lightweight'(true)
       }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
     }
   }
 }
+
+
+
