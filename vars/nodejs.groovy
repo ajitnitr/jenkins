ab72dc41dc86e689a448858b8755f9d57556c8de
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-05-04 14:17:05 +0530

    Update nodejs.groovy

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,25 +4,11 @@ def call() {
       label "${BUILD_LABEL}"
     }
 
-//    triggers {
-//      pollSCM('H/2 * * * *')
-//    }
-
-    environment {
-      PROG_LANG_NAME = "nodejs"
-      PROG_LANG_VERSION = "10"
+    triggers {
+      pollSCM('H/2 * * * *')
     }
 
     stages {
-
-      stage('Label Builds') {
-        steps {
-          script {
-            def gitTag = GIT_BRANCH.split('/').last()
-            addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${gitTag}"
-          }
-        }
-      }
 
 
       stage('Check the Code Quality') {
@@ -42,19 +28,13 @@ def call() {
       stage('Test Cases') {
         steps {
           sh 'echo Test Cases'
-          sh 'env'
         }
       }
 
       stage('Publish Artifacts') {
-        when {
-          expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
-        }
         steps {
-          script {
-            common.prepareArtifacts()
-            common.publishArtifacts()
-          }
+          sh 'echo Publish Artifacts'
+          sh 'env'
         }
       }
 
