6faa8a81da54695af4eb53c9ce960d3698da11f8
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-05-15 18:58:20 +0530

    Update seed.groovy

--- seed.groovy
+++ seed.groovy
@@ -52,5 +52,29 @@ for(i in 0..SIZE) {
   }
 }
 
+folder('Mutable') {
+  displayName('Mutable')
+  description('Mutable')
+}
 
+pipelineJob('Mutable/VPC') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/ajitnitr/terraform-vpc.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
 
